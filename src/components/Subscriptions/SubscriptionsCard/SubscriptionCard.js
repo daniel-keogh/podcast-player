2527b9bf085c181806a298e371f6fc13afcfb313
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2019-11-02 17:03:19 +0000

    Build subsciption card

---
+++ src/components/Subscriptions/SubscriptionsCard/SubscriptionCard.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardActionArea, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core'
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+const SubscriptionCard = (props) => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={props.podImage}
+          title={props.podName}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.podName}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.podDescription}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">View</Button>
+      </CardActions>
+    </Card>
+  );
+}
+
+export default SubscriptionCard;
