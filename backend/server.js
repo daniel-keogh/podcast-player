53f2fca1de6f45e0430f1095881255446f212e85
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2019-11-02 22:48:04 +0000

    Started discover section

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const app = express();
 const port = 4000;
 
+const axios = require('axios');
+
 app.use(cors());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -14,10 +16,20 @@ app.use((req, res, next) => {
 
 app.get('/api/subscriptions', (req, res) => {
     const subscriptions = JSON.parse(`{
-
-    `);
+        
+    }`);
 
     res.status(200).json({ subscriptions });
+});
+
+app.get('/api/top', (req, res) => {
+    axios.get(`https://rss.itunes.apple.com/api/v1/us/podcasts/top-podcasts/all/25/explicit.json`)
+        .then(data => {
+            return data.data.feed.results;
+        })
+        .then(top => {
+            res.status(200).send(top);
+        });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
--- src/components/Discover/Discover.js
+++ src/components/Discover/Discover.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import NavBar from '../NavBar/NavBar';
+import SubscriptionCard from '../Subscriptions/SubscriptionsCard/SubscriptionCard';
 
 class Discover extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+
+        this.state = {
+            top: ''
+        };
+    }
+
+    componentDidMount() {
+        axios.get(`http://localhost:4000/api/top`).then(data => {
+            if (data.status === 200) {
+                this.setState({
+                    top: data.data
+                });
+            }
+        });
     }
 
     render() {
+        let items;
+        if (this.state.top) {
+            items = this.state.top.map(item => {
+                return (
+                    <li key={item.id}>
+                        <SubscriptionCard
+                            artwork={item.artworkUrl100}
+                            podName={item.name}
+                            podAuthor={item.artistName}
+                        />
+                    </li>
+                );
+            });
+        }
+
         return (
-            <NavBar title="Discover" />
+            <React.Fragment>
+                <NavBar title="Discover" />
+                <div>
+                    <ul>
+                        {items}
+                    </ul>
+                </div>
+            </React.Fragment>
         );
     }
 }
