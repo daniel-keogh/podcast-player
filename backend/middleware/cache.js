072e045a9ffd2bdf5eefb200db28ab072d4a5b93
Daniel Keogh <37158241+daniel-keogh@users.noreply.github.com>
2021-12-27 13:25:28 +0000

    Refactor redis caching using a promises

--- backend/middleware/cache.js
+++ backend/middleware/cache.js
@@ -1,76 +1,73 @@
 const redisClient = require('../db/redis');
 
-exports.getCachedSubscription = (req, res, next) => {
-    if (!redisClient.connected) {
-        return next();
-    }
-
-    redisClient.get(req.params.id, (err, data) => {
-        if (err) {
-            err.status = 500;
-            throw err;
+const getCachedJSON = async (key) => {
+    return new Promise((resolve, reject) => {
+        if (!redisClient.connected) {
+            return reject();
         }
 
-        if (data) {
-            const { episodes, ...subscription } = JSON.parse(data);
-            const isSubscribed = req.user.subscriptions.indexOf(subscription._id) !== -1;
+        redisClient.get(key, (err, data) => {
+            if (err) {
+                err.status = 500;
+                throw err;
+            }
 
-            res.status(200).json({
-                ...subscription,
-                isSubscribed,
-                episodes: episodes.slice(0, req.query.limit),
-            });
-        } else {
-            next();
-        }
+            if (data) {
+                resolve(JSON.parse(data));
+            } else {
+                reject();
+            }
+        });
     });
 };
 
-exports.getCachedEpisodes = (req, res, next) => {
-    if (!redisClient.connected) {
-        return next();
+exports.getCachedSubscription = async (req, res, next) => {
+    const { id } = req.params;
+    const { limit } = req.query;
+    const user = req.user;
+
+    try {
+        const { episodes, ...subscription } = await getCachedJSON(id);
+
+        const isSubscribed = user.subscriptions.indexOf(subscription._id) !== -1;
+
+        res.status(200).json({
+            ...subscription,
+            isSubscribed,
+            episodes: episodes.slice(0, limit),
+        });
+    } catch (err) {
+        next(err);
     }
-
-    redisClient.get(req.params.id, (err, data) => {
-        if (err) {
-            err.status = 500;
-            throw err;
-        }
-
-        if (data) {
-            const { episodes } = JSON.parse(data);
-
-            res.status(200).json({
-                episodes: episodes.slice(0, req.query.limit),
-            });
-        } else {
-            next();
-        }
-    });
 };
 
-exports.getCachedEpisodeByGuid = (req, res, next) => {
-    if (!redisClient.connected) {
-        return next();
+exports.getCachedEpisodes = async (req, res, next) => {
+    const { id } = req.params;
+    const { limit } = req.query;
+
+    try {
+        const { episodes } = await getCachedJSON(id);
+
+        res.status(200).json({
+            episodes: episodes.slice(0, limit),
+        });
+    } catch (err) {
+        next(err);
     }
+};
 
-    redisClient.get(req.params.id, (err, data) => {
-        if (err) {
-            err.status = 500;
-            throw err;
-        }
+exports.getCachedEpisodeByGuid = async (req, res, next) => {
+    const { guid, id } = req.params;
 
-        if (data) {
-            const { episodes } = JSON.parse(data);
-            const guid = req.params.guid;
+    try {
+        const { episodes } = await getCachedJSON(id);
 
-            const result = episodes.filter((item) => item.guid === guid);
+        const result = episodes.filter((item) => item.guid === guid);
 
-            res.status(200).json({
-                episode: result?.length > 0 ? result[0] : {},
-            });
-        } else {
-            next();
-        }
-    });
+        res.status(200).json({
+            episode: result?.length > 0 ? result[0] : {},
+        });
+    } catch (err) {
+        next(err);
+    }
 };
